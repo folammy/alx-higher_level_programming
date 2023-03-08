7a66ef45c6554c7bb9320ed09ec411b514c49740
folammy <oluwabamisefola217@gmail.com>
2023-03-07 19:05:19 -0800

    if else loop functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Return the additin of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Returns the value of a raise to power b"""
+    return (a ** b)
